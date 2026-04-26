fd3ef843fb4ddba0f478c96cd46dcc184c9bff3a
Monique Pereira <monique.pereira@garantiabr.com>
2026-04-26 18:50:58 -0300

    Laboratório Kairo v2: Versão limpa e leve

---
+++ main.tf
@@ -0,0 +1,13 @@
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "kairo_v2_staging" {
+  ami           = "ami-0c55b159cbfafe1f0" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Kairo-V2-Staging"
+  }
+}
